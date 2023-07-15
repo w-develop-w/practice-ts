0b82abf03025a331a6e703ae02fdc35f61834b89
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-15 08:43:47 +0300

    logic in Home and hook debounce

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react"
 import { useSearchUsersQuery } from "../store/github/github.api"
+import { useDebounce } from "../hooks/debounce"
 
 export function HomePage() {
 
-    const {isLoading, isError, data} = useSearchUsersQuery('Vladilen')
+    const [search, setSearch] = useState('')
+    const debounced = useDebounce(search)
+    const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+        //information about when the request will not be sent
+        skip: debounced.length < 3
+    })
 
-    console.log(data)
+    useEffect(() => {
+        console.log(debounced)
+    }, [debounced])
 
     return (
-        <div>home</div>
+        <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+            {
+                isError && <p className="text-center text-red-600">Something went wrong...</p>
+            }
+
+            <div className="relative w-[560px">
+                <input type="text" className="border py-2 px-4 w-full h-[42px] mb-2"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search for Github username..."/>
+
+                <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+                    { isLoading && <p className="text-center">Loading...</p>} 
+                    { data?.map(user => (
+                        <li key={user.id} 
+                            className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                        >{ user.login }</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
